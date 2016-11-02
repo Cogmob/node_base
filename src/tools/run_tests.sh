fcee6fb944aa53955a3c6576f3cb2b676c842217
cogmob <luke.avery@live.co.uk>
2016-11-02 18:59:12 +0000

    remove unneeded sudos

--- src/tools/run_tests.sh
+++ src/tools/run_tests.sh
@@ -3,4 +3,4 @@ cp ../npm/gulpfile.js ../../gen/dev
 cd ../../gen/dev
 node_modules/.bin/gulp build_dev
 clear
-sudo node_modules/.bin/tape src/**/*test.js | sudo node_modules/.bin/tap-difflet
+node_modules/.bin/tape src/**/*test.js | node_modules/.bin/tap-difflet
