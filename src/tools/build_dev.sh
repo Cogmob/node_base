db1d046bf8123e1c5fdb1b2735eaf86476ca8b0c
cogmob <luke.avery@live.co.uk>
2016-11-07 11:48:50 +0000

    fixes found from making brace_switcher

--- src/tools/build_dev.sh
+++ src/tools/build_dev.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+mkdir -p ../../gen/dev
 cp ../npm/gulpfile.js ../../gen/dev
 cd ../../gen/dev
-cp ../../src/npm/package.json
+cp ../../src/npm/package.json .
 node_modules/.bin/gulp build_dev
