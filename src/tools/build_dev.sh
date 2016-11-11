d693496ef744cbfe3eb6f3fcbc6e5a3eb946be58
cogmob <luke.avery@live.co.uk>
2016-11-11 17:39:41 +0000

    src directory wasnt being deleted between compiles

--- src/tools/build_dev.sh
+++ src/tools/build_dev.sh
@@ -3,5 +3,5 @@ mkdir -p ../../gen/dev
 cp ../npm/gulpfile.js ../../gen/dev
 cd ../../gen/dev
 cp ../../src/npm/package.json .
-rm -rf source
+rm -rf src
 node_modules/.bin/gulp build_dev
--- src/tools/build_release.sh
+++ src/tools/build_release.sh
@@ -4,5 +4,6 @@ cd ../../gen/release
 cp ../../src/npm/package.json .
 cp ../../README.md .
 npm install
+rm -rf src
 clear
 node_modules/.bin/gulp build_release
